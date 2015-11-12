a0d4ff7e9120d8407289a5b64fffca722ffe357e
imadityab <imadityab@gmail.com>
2015-11-12 18:29:02 -0500

    adding build-VM directory.

---
+++ build-vm/bootstrap.sh
@@ -0,0 +1,15 @@
+yes | sudo apt-get update
+yes | sudo apt-get install git
+yes | sudo apt-get update
+yes | sudo apt-get install nodejs
+yes | sudo apt-get install npm
+#git clone https://github.com/Microsoft/TouchDevelop.git
+#cd TouchDevelop
+#sudo npm install jake -g
+#sudo npm install tsd@next -g
+#ln -s /usr/bin/nodejs /usr/bin/node
+#sudo tsd reinstall
+#sudo npm install
+#jake
+#jake local
+
